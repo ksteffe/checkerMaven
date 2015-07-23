ff60f559fef80402704d8ac9a96d6193050d8c1e
Kevin Steffensen <kevin.steffensen@overstock.com>
2015-07-23 14:31:10 -0600

    Updated Id to be a SubTypeOf Unqualified, and updated argfile to bring in Unqualified annotation.  Compilation now fails as expected.

--- checkerMaven-annotations/src/main/java/myqual/sample/annotations/Id.java
+++ checkerMaven-annotations/src/main/java/myqual/sample/annotations/Id.java
@@ -1,18 +1,22 @@
 package myqual.sample.annotations;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import org.checkerframework.framework.qual.DefaultQualifierInHierarchy;
 import org.checkerframework.framework.qual.SubtypeOf;
 import org.checkerframework.framework.qual.TypeQualifier;
+import org.checkerframework.framework.qual.Unqualified;
 
 /**
  * A type-safe Id marker
  */
 @TypeQualifier
-@DefaultQualifierInHierarchy
-@SubtypeOf({})
+@SubtypeOf({Unqualified.class})
 @Target({ElementType.TYPE_PARAMETER, ElementType.TYPE_USE})
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Id {
 }
